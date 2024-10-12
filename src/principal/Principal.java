a0c9c549882e88cd6be527507ec886ba531ed378
WillianXRosa <wxrosa@hotmail.com>
2024-10-12 07:53:26 -0300

    collections-java-api-2023 - Exercicios Catalogo de Livros - Testes.

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -8,7 +8,8 @@ public class Principal {
 	public static void main(String[] args) {		
 		
 		/**Teste da lista de tarefas**/
-		/*ListaTarefa listaTarefa = new ListaTarefa();
+		/**Instanciando a classe lista de tarefas**/
+		/*ListaTarefa listaTarefa = new ListaTarefa();		
 		
 		System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefa());
 		
@@ -26,12 +27,34 @@ public class Principal {
 		
 		
 		/**Teste do Catálogo de Livros**/
+		/**Book Catalog Test**/
+		
 		/**Instanciando a classe catalogo de livros**/
+		/**Instantiating the book catalog class**/
+		
 		CatalogoLivros objCatalogoLivros = new CatalogoLivros();
 		
 		/**Adicionando a livros**/
-		objCatalogoLivros.adicionarLivro("A Giripoca vai piar", "Mané", 1990);
-		objCatalogoLivros.adicionarLivro("Aprendendo Java", "José", 1995);
+		/**Adding to books**/
+		objCatalogoLivros.adicionarLivro("Livro1", "Autor1", 1990);
+		objCatalogoLivros.adicionarLivro("Livro1", "Autor2", 1997);
+		objCatalogoLivros.adicionarLivro("Livro2", "Autor2", 2000);
+		objCatalogoLivros.adicionarLivro("Livro3", "Autor3", 1971);
+		objCatalogoLivros.adicionarLivro("Livro4", "Autor4", 2023);
+		objCatalogoLivros.adicionarLivro("Livro5", "Autor4", 2017);
+		objCatalogoLivros.adicionarLivro("Livro6", "Autor6", 2013);
+		
+		/**Pesquisar livros por autor**/
+		/**Searching for books by author**/
+		System.out.println(objCatalogoLivros.pesquisarPorAutor("Autor4"));
+		
+		/**Pesquisar por intervalo de ano**/
+		/**Searching by year range**/
+		System.out.println(objCatalogoLivros.pesquisarPorIntervaloDeAno(1971, 1997));
+		
+		/**Pesquisar por título**/
+		/** Search By Title **/
+		System.out.println(objCatalogoLivros.pesquisarLivroPorTitulo("Livro1"));
 		
 		
 		
